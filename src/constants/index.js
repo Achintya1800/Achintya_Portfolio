bc55780513fde895a56b25f1db7a75cf86331105
Achintya1800 <achintya.chavan22@spit.ac.in>
2025-07-27 00:29:19 +0530

    Update Portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,267 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  Dashboard,
+  EyLogo,
+  mechhelplogo,
+  Skillsakhi,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  // {
+  //   title: "React.js Develeoper",
+  //   company_name: "Starbucks",
+  //   icon: starbucks,
+  //   iconBg: "#383E56",
+  //   date: "March 2020 - April 2021",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  // {
+  //   title: "React Native Developer",
+  //   company_name: "Tesla",
+  //   icon: tesla,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2021 - Feb 2022",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  {
+    title: "Software Developer Intern",
+    company_name: "Mechhelp",
+    icon: mechhelplogo,
+    iconBg: "#E6DEDD",
+    date: "Feb 2025 - May 2025",
+    points: [
+      "Developed a service portal using React and Tailwind CSS, improving user engagement by 30%.",
+      "Collaborated across teams in scrum meetings and actively contributed to sprint planning and feature decisions.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Strengthened skills in teamwork, Agile (Scrum), communication, and problem-solving during iterative feature delivery.",
+    ],
+  },
+  {
+    title: "Gen-AI Summer Intern",
+    company_name: "EY",
+    icon: EyLogo,
+    iconBg: "#E6DEDD",
+    date: "July 2025 - Present",
+    points: [
+      "Worked on developing an intelligent Retrieval-Augmented Generation (RAG) system for regulatory document analysis.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "ABHUDAYA",
+    description:
+      "Web application is a revolutionary platform that merges advanced AI with graph-based data modeling to simplify access to government welfare schemes.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "typescript",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: Dashboard,
+    source_code_link: "https://github.com/Achintya1800/ABHUDAYA",
+  },
+  {
+    name: "SkillSakhi",
+    description:
+      "SkillSakhi is an AI-powered learning and employment platform designed to empower underprivileged women through digital upskilling, personalized learning.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Python",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Nodejs",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: Skillsakhi,
+    source_code_link: "https://github.com/IEEECSAiravat/Airavat_QUISK_Asmiya-Sayyad",
+  },
+  {
+    name: "MERN Estate",
+    description:
+      "A comprehensive full-stack real estate marketplace built using the MERN stack with a modern UI powered and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "MongoDB",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Expressjs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Reactjs",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/Achintya1800/mern-estate",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
